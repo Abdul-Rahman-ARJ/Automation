88e491f2268deb93c6e22044e2ac7771c8bac902
Abdul Rahman <93648056+Abdul-Rahman-ARJ@users.noreply.github.com>
2023-08-28 12:54:10 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Define a function to simulate clicking 'Connect' buttons
+function clickConnectButtons() {
+  const connectButtons = document.querySelectorAll('.button-secondary-small');
+  
+  for (const button of connectButtons) {
+    button.click();
+    setTimeout(() => {
+      console.log("Clicked a 'Connect' button");
+    }, 2000); // Delay before clicking the next button
+  }
+}
+
+// Call the function to start clicking buttons
+clickConnectButtons();
